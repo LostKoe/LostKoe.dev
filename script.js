7fee4c0271e340dcfd7e363f40e772d58be86ed6
LostKoe <oscanero3@gmail.com>
2025-08-30 15:27:39 -0300

    Aviso de novo portfólio

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-        const btn = document.getElementById("hamburguer-appearance");
-        const menu = document.getElementById("hamburguer-items");
+  const btn = document.getElementById("hamburguer-appearance");
+  const menu = document.getElementById("hamburguer-items");
 
-        btn.addEventListener("click", () => {
-                menu.classList.toggle("active");
-        });
+  btn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+
+  alert("Há uma nova versão deste portfólio em lostkoe.github.io/Portfolio-LostKoe\nTambém aproveite e veja o repositório dele em github.com/LostKoe/Portfolio-LostKoe")
 });
